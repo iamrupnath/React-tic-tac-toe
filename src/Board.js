3df3d299a4f527dc612659c72305399edd837053
Rupnath Shaw <93729247+iamrupnath@users.noreply.github.com>
2023-05-02 10:24:03 +0530

    Add files via upload

---
+++ src/Board.js
@@ -0,0 +1,7 @@
+function board(props) {
+    return (
+        <div className="board" {...props} />
+    );
+}
+
+export default board;
---
+++ src/Square.js
@@ -0,0 +1,8 @@
+function Square(props) {
+    return (
+      <div className={'square'} {...props}>{props.x ? 'x' : (props.o ? 'o' : '')}</div>
+    );
+  }
+  
+  export default Square;
+  
